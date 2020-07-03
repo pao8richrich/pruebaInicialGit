390f1c48bbccbef7da79f03af4a8e63436c35653
srico <srico@quantumconnexion.com>
2020-07-02 21:32:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+          esta es una prueba
+          
         </p>
         <a
           className="App-link"
